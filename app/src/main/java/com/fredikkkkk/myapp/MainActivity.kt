493a2bf70896d2e6409e24cb85420e22fb861834
fredikkkkk <tishkovskiyyan@gmail.com>
2025-11-10 19:13:10 +0300

    initial commit

--- app/src/main/java/com/fredikkkkk/myapp/MainActivity.kt
+++ app/src/main/java/com/fredikkkkk/myapp/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
 import com.fredikkkkk.myapp.ui.theme.MyAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -34,6 +35,7 @@ class MainActivity : ComponentActivity() {
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
         text = "Hello $name!",
+        color = Color.Red,
         modifier = modifier
     )
 }
